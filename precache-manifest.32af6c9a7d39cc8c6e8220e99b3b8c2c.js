f02ae3d75d1c3976bf93406e2e558908848a5382
ftnism <31004540+ftnism@users.noreply.github.com>
2020-06-16 17:06:39 +0600

    Updates

---
+++ precache-manifest.32af6c9a7d39cc8c6e8220e99b3b8c2c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da4cc1399766d0b50b0f5d22fe4c3d56",
+    "url": "/GitHub-Education-Portfolio/index.html"
+  },
+  {
+    "revision": "fed7c3a896fee985f7a4",
+    "url": "/GitHub-Education-Portfolio/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "7d9747e8ed579763acf5",
+    "url": "/GitHub-Education-Portfolio/static/js/2.2fd86e5d.chunk.js"
+  },
+  {
+    "revision": "fed7c3a896fee985f7a4",
+    "url": "/GitHub-Education-Portfolio/static/js/main.058fd9cd.chunk.js"
+  },
+  {
+    "revision": "5e7d53efc42e27e2deba",
+    "url": "/GitHub-Education-Portfolio/static/js/runtime~main.60ee6b4b.js"
+  },
+  {
+    "revision": "f2dd38cad44349cec2eaf4ead9b14321",
+    "url": "/GitHub-Education-Portfolio/static/media/profile.f2dd38ca.jpg"
+  }
+]);
